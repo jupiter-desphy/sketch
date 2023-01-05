8b3ff30ef9e95d3b4fcfcfba162ce53b4a9b642a
Jupiter Desphy <jupiterdesphy7@gmail.com>
2023-01-04 19:30:45 -0800

    made drawings more randomized and dramatic

--- sketch-02-z.js
+++ sketch-02-z.js
@@ -1,38 +1,57 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [1080, 1080],
 };
 
 const degToRad = (degrees) => {
-  return degrees / 180 * Math.PI;
-}
+  return (degrees / 180) * Math.PI;
+};
+
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
     context.fillStyle = "black";
 
-    const x = width * 0.5;
-    const y = height * 0.5;
+    const cx = width * 0.5;
+    const cy = height * 0.5;
     const w = width * 0.01;
     const h = height * 0.1;
 
-    const num = 12;
+    const num = random.range(12, 120);
+    const radius = width * 0.3;
 
     for (i = 0; i < num; i++) {
-
       const slice = degToRad(360) / num;
       const angle = slice * i;
 
+      let x = cx + radius * Math.sin(angle);
+      let y = cy + radius * Math.cos(angle);
+
       context.save();
       context.translate(x, y);
-      context.rotate(angle);
+      context.rotate(-angle);
+      context.scale(random.range(.1, 2), random.range(3, -3));
+
+      context.beginPath();
+      context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
+      context.fill();
+      context.restore();
+
+      context.save();
+      context.translate(cx, cy);
+      context.rotate(-angle);
+
+      context.lineWidth = random.range(5, 20);
       
       context.beginPath();
-      context.rect(-w * .5, -h * .5, w, h);
-      context.fill();
+      context.arc(0, 0, radius *random.range(0.7, 2.5), slice * random.range(1, -8), slice * random.range(0, 5));
+      context.stroke();
+
       context.restore();
     }
   };
