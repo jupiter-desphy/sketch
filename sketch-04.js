5fc3d9b49cf9804f5cd5e4b3323439a2337b773d
Jupiter Desphy <jupiterdesphy7@gmail.com>
2023-01-06 20:24:25 -0800

    drew lines. bumped rows and cols to 10 x 10.

--- sketch-04.js
+++ sketch-04.js
@@ -1,30 +1,48 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [1080, 1080],
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    const cols = 4;
-    const rows = 3;
+    const cols = 10;
+    const rows = 10;
     const numCells = cols * rows;
 
     const gridW = width * 0.8;
     const gridH = height * 0.8;
     const cellW = gridW / cols;
     const cellH = gridH / rows;
-    const margX = (width - gridW) * .5;
-    const margY = (height - gridH) * .5;
+    const margX = (width - gridW) * 0.5;
+    const margY = (height - gridH) * 0.5;
 
     for (i = 0; i < numCells; i++) {
       const col = i % cols;
       const row = Math.floor(i / cols);
-    }
 
+      const x = col * cellW;
+      const y = row * cellH;
+      const w = cellW * 0.8;
+      const h = cellH * 0.8;
+
+      context.save();
+      context.translate(x, y);
+      context.translate(margX, margY);
+      context.translate(cellW * .5, cellH * .5)
+
+      context.lineWidth = 4;
+
+      context.beginPath();
+      context.moveTo(w * -0.5, 0);
+      context.lineTo(w * 0.5, 0);
+
+      context.stroke();
+      context.restore();
+    }
   };
 };
 
