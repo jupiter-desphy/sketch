a5314cfbca57009d068ef9c127b005a9878c8392
Jupiter Desphy <jupiterdesphy7@gmail.com>
2023-01-08 21:31:15 -0800

    update control panel

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # sketch
+# Snake
+\* *  *Technologies used include* ***React***, ***JavaScript***, ***HTML***, *and* ***CSS***. * * 
+
+<!-- [<img width="900px" src="public/images/screenshot.png"/>](https://nokia-snake-jupiter-desphy.vercel.app/) -->
+
+<!--
+  [<img width="800px" src="public/images/ScrShotFiltered.png"/>](https://nokia-snake-jupiter-desphy.vercel.app/snake1)
+-->
+
+## Contact Me
+<!-- [![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://nokia-snake-jupiter-desphy.vercel.app/) -->
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/jupiterdesphy/)
+
+## Acknowledgements
+ - 
+ - Canvas Sketch by Matt DesLauriers [@mattdesl](https://github.com/mattdesl/canvas-sketch).
+ - Tweakpane by [Hiroki Kokubun](https://github.com/cocopon/tweakpane).
--- sketch-04.js
+++ sketch-04.js
@@ -1,18 +1,29 @@
 const canvasSketch = require("canvas-sketch");
 const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
+const Tweakpane = require('tweakpane');
 
 const settings = {
   dimensions: [1080, 1080],
   animate: true
 };
 
+const params = {
+  cols: 10,
+  rows: 10,
+  scaleMin: 1,
+  scaleMax: 30,
+  freq: 0.001,
+  amp: 0.2,
+}
+
 const sketch = () => {
   return ({ context, width, height, frame }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    const cols = 10;
-    const rows = 10;
+    const cols = params.cols;
+    const rows = params.rows;
     const numCells = cols * rows;
 
     const gridW = width * 0.8;
@@ -31,10 +42,12 @@ const sketch = () => {
       const w = cellW * 0.8;
       const h = cellH * 0.8;
 
-      const n = random.noise2D(x + frame * 15, y, .001);
-      const angle = n * Math.PI * .2;
-      const scale = (n + 1) * .5 * 30;
-
+      // const n = random.noise2D(x + frame * 15, y, params.freq);
+      const n = random.noise3D(x, y, frame * 15, params.freq);
+      const angle = n * Math.PI * params.amp;
+      
+      // const scale = (n + 1) * .5 * 30;
+      const scale = math.mapRange(n, -1, 1, params.scaleMin, params.scaleMax);
       context.save();
       context.translate(x, y);
       context.translate(margX, margY);
@@ -53,4 +66,22 @@ const sketch = () => {
   };
 };
 
+const createPane = () => {
+  const pane = new Tweakpane.Pane();
+  let folder;
+
+  folder = pane.addFolder({
+    title: 'Grid '
+  });
+  folder.addInput(params, 'cols', { min: 2, max: 50, step: 1 });
+  folder.addInput(params, 'rows', { min: 2, max: 50, step: 1 });
+  folder.addInput(params, 'scaleMin', { min: 1, max: 100 });
+  folder.addInput(params, 'scaleMax', { min: 1, max: 100 });
+
+  folder = pane.addFolder({ title: 'Noise' });
+  folder.addInput(params, 'freq', { min: -0.01, max: 0.01 });
+  folder.addInput(params, 'amp', { min: 0, max: 1 });
+};
+
+createPane();
 canvasSketch(sketch, settings);
